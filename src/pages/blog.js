d308e716066aba85ac6ac58f1edaaefcf982d96c
Juan Carrera <juankarrera@gmail.com>
2020-01-14 23:58:55 -0500

    change style in blog component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -14,7 +14,6 @@ const BlogPage = ({ data }) => (
         <h3>{post.node.frontmatter.title}</h3>
         <small>Posted by {post.node.frontmatter.author} on {post.node.frontmatter.date}</small>
         <br />
-        <br />
         <Link to={post.node.frontmatter.path}>Read More</Link>
         <br />
         <br />
