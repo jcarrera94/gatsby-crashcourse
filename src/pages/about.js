63144b384aed75edca57752cb9263d7d00c79552
Juan Carrera <juankarrera@gmail.com>
2020-01-14 19:38:39 -0500

    add about us page

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <SEO title="About Us" />
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, nulla doloremque, aperiam odio reprehenderit dolorum, sunt amet tempora expedita similique voluptates. Ut quasi nisi suscipit reiciendis necessitatibus architecto tenetur iusto.
+      </p>
+    </Layout>
+  )
+}
+
+export default AboutPage;
