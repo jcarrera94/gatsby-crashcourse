6dda857028fb1b60d4fd47dfd05602bd8edc5fbe
Juan Carrera <juankarrera@gmail.com>
2020-01-14 19:58:22 -0500

    add services page

---
+++ src/pages/services.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const ServicesPage = () => {
+  return (
+    <Layout>
+      <SEO title="Services" />
+      <h1>Our Services</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, nulla doloremque, aperiam odio reprehenderit dolorum, sunt amet tempora expedita similique voluptates. Ut quasi nisi suscipit reiciendis necessitatibus architecto tenetur iusto.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, nulla doloremque, aperiam odio reprehenderit dolorum, sunt amet tempora expedita similique voluptates. Ut quasi nisi suscipit reiciendis necessitatibus architecto tenetur iusto.
+      </p>
+    </Layout>
+  )
+}
+
+export default ServicesPage;
