8c29612c863a5cbd069e80f4cc94094717d553f5
Juan Carrera <juankarrera@gmail.com>
2020-01-14 22:41:51 -0500

    create menu component including links to other pages

--- src/components/header.js
+++ src/components/header.js
@@ -2,33 +2,38 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import Menu from "../components/menu"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#333`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+  <div>
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `#333`,
+        marginBottom: `none`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+    </header>
+    <Menu />
+  </div>
 )
 
 Header.propTypes = {
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,16 +3,15 @@ import React from 'react';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const AboutPage = () => {
-  return (
-    <Layout>
-      <SEO title="About Us" />
-      <h1>About Us</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, nulla doloremque, aperiam odio reprehenderit dolorum, sunt amet tempora expedita similique voluptates. Ut quasi nisi suscipit reiciendis necessitatibus architecto tenetur iusto.
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About Us" />
+    <h1>About Us</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, nulla doloremque, aperiam odio reprehenderit dolorum, sunt amet tempora expedita similique voluptates. Ut quasi nisi suscipit reiciendis necessitatibus architecto tenetur iusto.
       </p>
-    </Layout>
-  )
-}
+  </Layout>
+)
+
 
 export default AboutPage;
